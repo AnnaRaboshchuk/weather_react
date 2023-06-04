758422b56ee85f29931ecfcf1d92f7a0ca0796f2
AnnaRaboshchuk <anna.raboshchuk@gmail.com>
2023-06-04 18:10:52 +0200

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project is coded by Anna Raboshchuk and is{" "}
+        <a
+          href="https://github.com/AnnaRaboshchuk/weather_react"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
